0201a73d3e1fa9bd1f787607a008bfe2eb71ee55
puppe1990 <matheus.puppe90@hotmail.com>
2023-01-14 10:38:40 -0300

    add some rules

--- src/App.js
+++ src/App.js
@@ -1,29 +1,27 @@
-import React, { useState, useEffect } from 'react';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
 
 const POINTS = [
-  'Karate chop',
-  'Top of head',
-  'Eyebrow',
-  'Side of eye',
-  'Under eye',
-  'Under nose',
-  'Chin',
-  'Collarbone',
-  'Chest',
-  'Underarm'
+  "Karate chop",
+  "Top of head",
+  "Eyebrow",
+  "Side of eye",
+  "Under eye",
+  "Under nose",
+  "Chin",
+  "Collarbone",
+  "Chest",
+  "Underarm",
 ];
 
 const CUSTOM_PHRASES = [
-  "Even though I have this fear of public speaking, I deeply and completely accept myself.",
-  "Even though I have this headache, I deeply and completely accept myself.",
-  "Even though I have this traumatic memory, I deeply and completely accept myself."
+  'Even though I struggle with financial abundance, I deeply and completely accept myself.','I have a hard time believing I deserve financial abundance.',"I feel like I'm not worthy of abundance.","I don't know how to attract abundance.","I'm scared that I'll never have enough money.","I'm always worried about money.","I don't trust that I can have abundance.","I don't believe I deserve abundance.","I'm worried that I'll never be able to afford the things I want.","I'm always struggling to make ends meet."
 ];
 
 const MOTIVATIONAL_PHRASES = [
   "You've made a great progress today!",
   "Keep up the good work!",
-  "You're getting stronger every day!"
+  "You're getting stronger every day!",
 ];
 
 function App() {
@@ -37,30 +35,46 @@ function App() {
   const handleNext = () => {
     const nextIndex = CUSTOM_PHRASES.indexOf(currentPhrase) + 1;
     if (nextIndex === CUSTOM_PHRASES.length) {
-    setCurrentPhrase(CUSTOM_PHRASES[0]);
+      setCurrentPhrase(CUSTOM_PHRASES[0]);
     } else {
-    setCurrentPhrase(CUSTOM_PHRASES[nextIndex]);
+      setCurrentPhrase(CUSTOM_PHRASES[nextIndex]);
     }
     const nextPoint = POINTS.indexOf(currentPoint) + 1;
-if (nextPoint === POINTS.length) {
-  setCurrentPoint(POINTS[0]);
-} else {
-  setCurrentPoint(POINTS[nextPoint]);
-}
+    if (nextPoint === POINTS.length) {
+      setCurrentPoint(POINTS[0]);
+    } else {
+      setCurrentPoint(POINTS[nextPoint]);
+    }
 
-if (!startTime) {
-  setStartTime(Date.now());
-}
-};
-
-const handleFeelingChange = (feeling) => {
-  setCurrentFeeling(feeling);
-  if (feeling === 0) {
-  setEndTime(Date.now());
-  setIsRoutineActive(false);
-  }
+    if (!startTime) {
+      setStartTime(Date.now());
+    }
   };
-  
+
+  const handleFeelingChange = (feeling) => {
+    setCurrentFeeling(feeling);
+    if (feeling === 0) {
+      setEndTime(Date.now());
+      setIsRoutineActive(false);
+    }
+  };
+
+  useEffect(() => {
+    if (isRoutineActive) {
+      let intervalId = null;
+      if (currentPoint === "Karate chop") {
+        intervalId = setInterval(() => {
+          handleNext();
+        }, 12000);
+      } else {
+        intervalId = setInterval(() => {
+          handleNext();
+        }, 7000);
+      }
+      return () => clearInterval(intervalId);
+    }
+  }, [isRoutineActive, handleNext, currentPoint]);
+
   useEffect(() => {
     if (isRoutineActive) {
       const intervalId = setInterval(() => {
@@ -69,39 +83,38 @@ const handleFeelingChange = (feeling) => {
       return () => clearInterval(intervalId);
     }
   }, [isRoutineActive, handleNext]);
-  
-  useEffect(() => {
-  if (isRoutineActive) {
-  const intervalId = setInterval(() => {
-  handleNext();
-  }, 3000);
-  return () => clearInterval(intervalId);
-  }
-  }, [isRoutineActive, handleNext]);
-  
+
   if (!isRoutineActive) {
-  const timeTaken = (endTime - startTime) / 1000; // in seconds
-  const randomPhrase = MOTIVATIONAL_PHRASES[Math.floor(Math.random() * MOTIVATIONAL_PHRASES.length)];
-  return (
-  <div className="App">
-  <h1>EFT Tapping</h1>
-  <h2>{randomPhrase}</h2>
-  <h2>Time taken: {timeTaken} seconds</h2>
-  </div>
-  );
+    const timeTaken = (endTime - startTime) / 1000; // in seconds
+    const randomPhrase =
+      MOTIVATIONAL_PHRASES[
+        Math.floor(Math.random() * MOTIVATIONAL_PHRASES.length)
+      ];
+    return (
+      <div className="App">
+        <h1>EFT Tapping</h1>
+        <h2>{randomPhrase}</h2>
+        <h2>Time taken: {timeTaken} seconds</h2>
+      </div>
+    );
   }
-  
+
   return (
-  <div className="App">
-  <h1>EFT Tapping</h1>
-  <h2 class="point">Current Point: {currentPoint}</h2>
-  <h2 class="phrase">Current Phrase: {currentPhrase}</h2>
-  <label>
-  On a scale from 0 to 10, how do you feel now?
-<input type="number" min="0" max="10" onChange={e => handleFeelingChange(e.target.value)} />
-</label>
-</div>
-);
+    <div className="App">
+      <h1>EFT Tapping</h1>
+      <h2 class="point">Current Point: {currentPoint}</h2>
+      <h2 class="phrase">Current Phrase: {currentPhrase}</h2>
+      <label>
+        On a scale from 0 to 10, how do you feel now?
+        <input
+          type="number"
+          min="0"
+          max="10"
+          onChange={(e) => handleFeelingChange(e.target.value)}
+        />
+      </label>
+    </div>
+  );
 }
 
 export default App;
