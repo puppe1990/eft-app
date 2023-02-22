98a169fad882ee6f4881334cc8eca52129ae8129
puppe1990 <matheus.puppe90@hotmail.com>
2023-02-22 12:59:56 -0300

    add phrase

--- src/App.js
+++ src/App.js
@@ -15,9 +15,8 @@ const POINTS = [
 ];
 
 const CUSTOM_PHRASES = [
-
-];
-
+  'Even though I struggle with financial abundance, I deeply and completely accept myself.','I have a hard time believing I deserve financial abundance.',"I feel like I'm not worthy of abundance.","I don't know how to attract abundance.","I'm scared that I'll never have enough money.","I'm always worried about money.","I don't trust that I can have abundance.","I don't believe I deserve abundance.","I'm worried that I'll never be able to afford the things I want.","I'm always struggling to make ends meet."
+]
 function App() {
   const [currentPhrase, setCurrentPhrase] = useState(CUSTOM_PHRASES[0]);
   const [currentPoint, setCurrentPoint] = useState(POINTS[0]);
